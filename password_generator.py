d42e082183e02847e4f1452aed8c423d83890772
Chris388383 <cr86343p@pace.edu>
2025-04-16 18:41:57 -0400

    Update password_generator.py
    Completed code

--- password_generator.py
+++ password_generator.py
@@ -1,37 +1,26 @@
-import random
-import string
+import util
 import streamlit as st
 
 
 # Things to do: have a slider for password length
 #add a check state
-#store the password generator logic in modeule
-#have a "copy to clipboard"icon to directly copy the password
+#store the password generator logic in module
+#have a "copy to clipboard" icon to directly copy the password
 
+st.title("Password Generator")
 
-password_length = st.number_input("Enter Password Length", min_value=0, max_value=100, step=1) #number of characters
+password_length = st.slider("Enter Password Length", min_value=0, max_value=100, step=1) #number of characters
 capital_letters = st.number_input("Enter Number of Capital Letters", min_value=0, max_value=100, step=1) #number of capital letters
 special_characters = st.number_input("Enter Number of Characters", min_value=0, max_value=100, step=1) #number of special characters
 
 # Generate password button
 if st.button("Generate Password!"):
 
-
-    # Create character pools
-    lowercase = string.ascii_lowercase
-    uppercase = string.ascii_uppercase
-    special = "!@#$%^&*()_+-=[]{}|;:,.<>?"
-
-
-    # Generate components
-    caps = ''.join(random.choices(uppercase, k=capital_letters))
-    specs = ''.join(random.choices(special, k=special_characters))
-    remaining_length = password_length - capital_letters - special_characters
-    lowers = ''.join(random.choices(lowercase, k=remaining_length))
-
-
-    # Combine and shuffle
-    password_list = list(caps + specs + lowers)
-    random.shuffle(password_list)
-    password = ''.join(password_list)
-    st.write(f"Generated password: {password}")
+    # Checks if number of characters and capital letters do not exceed password length
+    if password_length < capital_letters + special_characters:
+        st.warning("Number of characters/letters exceed password length")
+    else:
+        #Calls function from util.py file and writes password in streamlit
+        password = util.generate_password(password_length, capital_letters, special_characters)
+      
+        st.write(f"Generated password: {password}")
